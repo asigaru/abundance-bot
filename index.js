a8ce7725470e788f743ef941dc7b3b7d1a0613a6
Argali <arkharovp@gmail.com>
2020-07-17 14:42:51 +0300

    Rewrote the functions and logic. Fixed the errors led to crushes in production.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+
+// Connecting general dependencies 
+
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
+const firebase = require ('firebase')
+const TelegramBot = require('node-telegram-bot-api')
 
+// Connecting internal data files and development scripts
+
+// const debug = require('./helpers')
 const tasks = require('./tasks')
 const audio = require('./audio')
-const TelegramBot = require('node-telegram-bot-api')
-const debug = require('./helpers')
-const firebase = require ('firebase')
 
 console.log('Bot has been started ')
 
+// Initializing Firebase
 const app = firebase.initializeApp({
     apiKey: "AIzaSyBZiGQXKBM6MDIyScHJGdCHoiZCiLiLXSQ",
     authDomain: "abundancebot.firebaseapp.com",
@@ -20,79 +26,101 @@ const app = firebase.initializeApp({
     measurementId: "G-0RBR9YEX3V"
 });
 
-
+// Pointing Firebase DataBase references to variables
 const ref = firebase.database().ref();
 const usersRef = ref.child('users');
+
+// Initializing bot object with default settings
 
 const bot = new TelegramBot(TOKEN, {
     polling: true
         });
 
+        // Catching polling errors
 bot.on("polling_error", (err) => console.log(err));
 
 
-// Getting audio IDs here:
- //bot.on('message', msg => {
-   //  const { id } = msg.chat;
-   //  bot.sendMessage(id, debug(msg));
-//})
-
-
-
   bot.onText(/\/begin/, msg => {
+// Creating local variable referencing details of incoming /begin command
       const { id } = msg.chat
-// Starting out with sending out task for the day 0
+// Sending out task for the day 0
       bot.sendMessage(id, tasks[0]);
-// Creating a database entry for user in Firebase
+// Creating a database entry with telegram uId as key
       usersRef.child(id).set({ 
         name: msg.from.first_name,
         day: 1,
         date: msg.date
       });
-      //bot.sendMessage(id, debug(msg));//
-      bot.sendMessage(id,'You will recieve your first task within 24 hours. Now, rest.');
+      bot.sendMessage(id,"\n*\n*\n*\nType /ready to recieve your next day's task and audio file.");
 
 
   });
+ 
   
-function loadDate( id ) {
-  usersRef.on('date', function(snapshot) {
-  pastDate = (snapshot.val());
-  return pastDate;
-})
+// Function that retrieves date data from DB with snapshot function
+
+async function getDate( id ) {
+
+  usersRef.child(id).once('value').then(function(snapshot) {
+    const date = snapshot.val().date;
+
+  // If data is successfully retrieved from DB and variable is populated - proceed to checkDate function; otherwise throw an error
+    if (date) {
+      checkDate( id, date );
+    }
+  }).catch(function(error) {
+    console.log('Unable to get date: ' + error);
+  })
 };
 
-  function loadData( id ) {
-    // Recieving current day number from Firebase 
-            currentDayRef = usersRef.child(id).child('day');
-            currentDayRef.transaction(function(currentDay) {
-    // Sending out daily task and updating day number in Firebase
-                bot.sendMessage(id, tasks[currentDay]);
-                bot.sendAudio(id, audio[currentDay - 1] );
-                return currentDay + 1;
-            })
-        };
+// Callback function that checks that user sent confirmation of completing tasks within 24h limit
 
+  function checkDate( id, date ) {
 
-  function checkDate( id ) {
-    pastDate = loadDate( id );
-    dateNow = new Date();
-    if (dateNow - msg.date <= 86400000) {
-        loadData();
+    dateNow = Date.now();
+    let diff = dateNow - date;
+
+    console.log('date now is ' + dateNow);
+    console.log('Past date is ' + date);
+    console.log('Time difference is');
+    console.log(diff);
+    
+    if (diff >= 86400000) {
+       loadData( id );
     } else {
         bot.sendMessage(id, 'You were too late. Type /begin to start again.');
     }
   }
 
+  // Function to communicate with Firebase DB 
+  function loadData( id ) {
+    // Assigning reference to DB entry to variable
+            currentDayRef = usersRef.child(id).child('day');
+    // Reading data from DB with snapshot function
+            currentDayRef.once('value').then(function(snapshot) {
+    // Trying to assign value from DB to a local variable
+                const currentDay = snapshot.val();
+    // When and if data read succesfully 
+              if (currentDay) {
+                bot.sendMessage(id, tasks[currentDay]).then(() => {
+                  bot.sendAudio(id, audio[currentDay - 1] )
+                })   // Here sendMessages are chained with .then to make sure that audio file arrives after text
+
+    // Updating DB with new day and date values after sending the tasks to user
+                const update = {};
+                update['/users/' + id + '/day'] = currentDay + 1;
+                update['/users/' + id + '/date'] = Date.now();
+                firebase.database().ref().update(update);
+              }
+            }).catch(function(error) {
+              console.log('Unable to load data: ' + error);
+            }) // Catching possible errors
+        };
   
-    
-bot.onText(/\/dailytaskdone/, msg => {
+    // RegExp trigger for sending daily task and updating DB entry
+bot.onText(/\/ready/, msg => {
     const { id } = msg.chat
-    console.log('YO');
-    
-    loadData( id );
-    console.log('YOY');
+    getDate( id );
 })
 
-console.log('YOYOYO')
   
